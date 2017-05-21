b16027374e131f396e028628b8240ca616c58e06
mattdoingwork <clampitt.matthew@gmail.com>
2017-05-20 21:10:13 -0500

    temp remove prcommit lint

--- src/components/widgets/BaseInput.js
+++ src/components/widgets/BaseInput.js
@@ -1,60 +1,27 @@
-import React from "react";
-import PropTypes from "prop-types";
+import Inferno from "inferno";
 
-function BaseInput(props) {
-  // Note: since React 15.2.0 we can't forward unknown element attributes, so we
-  // exclude the "options" and "schema" ones here.
-  const {
-    value,
-    readonly,
-    disabled,
-    autofocus,
-    onBlur,
-    options,
-    schema,
-    formContext,
-    registry,
-    ...inputProps
-  } = props;
+const BaseInput = props => {
+  const { ...inputProps } = props;
 
-  inputProps.type = options.inputType || inputProps.type || "text";
   const _onChange = ({ target: { value } }) => {
-    return props.onChange(value === "" ? options.emptyValue : value);
+    return props.onChange(value === "" ? props.options.emptyValue : value);
   };
+
   return (
     <input
       className="form-control"
-      readOnly={readonly}
-      disabled={disabled}
-      autoFocus={autofocus}
-      value={value == null ? "" : value}
+      readOnly={props.readonly}
+      disabled={props.disabled}
+      autoFocus={props.autofocus}
+      value={props.value === null ? "" : props.value}
       {...inputProps}
       onChange={_onChange}
-      onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
+      onBlur={
+        props.onBlur &&
+          (event => props.onBlur(inputProps.id, event.target.value))
+      }
     />
   );
-}
-
-BaseInput.defaultProps = {
-  type: "text",
-  required: false,
-  disabled: false,
-  readonly: false,
-  autofocus: false,
 };
-
-if (process.env.NODE_ENV !== "production") {
-  BaseInput.propTypes = {
-    id: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
-    value: PropTypes.any,
-    required: PropTypes.bool,
-    disabled: PropTypes.bool,
-    readonly: PropTypes.bool,
-    autofocus: PropTypes.bool,
-    onChange: PropTypes.func,
-    onBlur: PropTypes.func,
-  };
-}
 
 export default BaseInput;
